29713fd5413fc562a7f3cd7537e32a2ac4f323d4
Fabio Curtis Volpe <curtis.fabio@gmail.com>
2026-02-22 15:13:44 -0300

    serviço cliente ajustes

--- cliente/src/main/java/com/adjt/rest/controller/ClienteController.java
+++ cliente/src/main/java/com/adjt/rest/controller/ClienteController.java
@@ -1,15 +1,12 @@
 package com.adjt.rest.controller;
 
 import com.adjt.core.model.Cliente;
-import com.adjt.core.usecase.cliente.CriarClienteUseCase;
+import com.adjt.core.usecase.cliente.*;
 import com.adjt.rest.dto.request.ClienteRequest;
 import com.adjt.rest.dto.response.ClienteResponse;
 import com.adjt.rest.mapper.ClienteRestMapper;
 import jakarta.validation.Valid;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
@@ -19,11 +16,21 @@ import jakarta.ws.rs.core.Response;
 public class ClienteController {
 
     private final CriarClienteUseCase criarClienteUseCase;
+    private final AtualizarClienteUseCase atualizarClienteUseCase;
+    private final ObterPorIdClienteUseCase obterPorIdClienteUseCase;
+    private final ExcluirClienteUseCase excluirClienteUseCase;
+
     private final ClienteRestMapper clienteRestMapper;
 
     public ClienteController(CriarClienteUseCase criarClienteUseCase,
+                             AtualizarClienteUseCase atualizarClienteUseCase,
+                             ObterPorIdClienteUseCase obterPorIdClienteUseCase,
+                             ExcluirClienteUseCase excluirClienteUseCase,
                              ClienteRestMapper clienteRestMapper) {
         this.criarClienteUseCase = criarClienteUseCase;
+        this.atualizarClienteUseCase = atualizarClienteUseCase;
+        this.obterPorIdClienteUseCase = obterPorIdClienteUseCase;
+        this.excluirClienteUseCase = excluirClienteUseCase;
         this.clienteRestMapper = clienteRestMapper;
     }
 
@@ -31,9 +38,36 @@ public class ClienteController {
     public Response criar(@Valid ClienteRequest request) {
 
         Cliente model = this.clienteRestMapper.toModel(request);
-        Cliente resp = criarClienteUseCase.run(model);
+        Cliente resp = this.criarClienteUseCase.run(model);
         ClienteResponse response = this.clienteRestMapper.toResponse(resp);
 
         return Response.status(Response.Status.CREATED).entity(response).build();
+    }
+
+    @PUT
+    public Response atualizar(@Valid ClienteRequest request) {
+
+        Cliente model = this.clienteRestMapper.toModel(request);
+        Cliente resp = this.atualizarClienteUseCase.run(model);
+        ClienteResponse response = this.clienteRestMapper.toResponse(resp);
+
+        return Response.status(Response.Status.CREATED).entity(response).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response obter(@PathParam("id") @Valid Long id) {
+
+        Cliente resp = this.obterPorIdClienteUseCase.run(id);
+        ClienteResponse response = this.clienteRestMapper.toResponse(resp);
+
+        return Response.status(Response.Status.CREATED).entity(response).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response excluir(@PathParam("id") @Valid Long id) {
+        Boolean resp = this.excluirClienteUseCase.run(id);
+        return Response.status(Response.Status.CREATED).entity(resp).build();
     }
 }
