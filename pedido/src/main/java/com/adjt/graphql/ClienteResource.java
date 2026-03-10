91c74cb62919ede1dbaf693c6d0f6006c67c331a
Fabio Curtis Volpe <curtis.fabio@gmail.com>
2026-03-10 07:47:42 -0300

    ajustes pedido gprc e graphql

--- pedido/src/main/java/com/adjt/graphql/ClienteResource.java
+++ pedido/src/main/java/com/adjt/graphql/ClienteResource.java
@@ -2,13 +2,14 @@ package com.adjt.graphql;
 
 import com.adjt.core.model.ClienteInfo;
 import com.adjt.gprc.ClienteGrpcUseCase;
+import io.smallrye.common.annotation.Blocking;
 import jakarta.annotation.security.RolesAllowed;
 import org.eclipse.microprofile.graphql.GraphQLApi;
 import org.eclipse.microprofile.graphql.Mutation;
 import org.eclipse.microprofile.graphql.Query;
 
 @GraphQLApi
-//@RolesAllowed("ROLE_CLIENTE")
+@RolesAllowed("ROLE_CLIENTE")
 public class ClienteResource {
 
     private final ClienteGrpcUseCase clienteGrpcUseCase;
@@ -18,12 +19,14 @@ public class ClienteResource {
     }
 
     @Query("buscarCliente")
+    @Blocking
     public ClienteInfo buscarCliente(Long id) {
         return clienteGrpcUseCase.buscarClientePorId(id)
                 .await().indefinitely();
     }
 
     @Mutation("atualizarCliente")
+    @Blocking
     public ClienteInfo atualizarCliente(ClienteInfo clienteInfo) {
         return clienteGrpcUseCase.atualizarCliente(clienteInfo)
                 .await().indefinitely();
